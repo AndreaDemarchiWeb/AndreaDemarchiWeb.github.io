2deb671093e05a473eea776e3aba84d73343bcc3
SirEversor <118366023+AndreaDemarchiWeb@users.noreply.github.com>
2023-01-25 15:00:21 +0100

    Added JS file

---
+++ assets/js/myscript.js
@@ -0,0 +1,10 @@
+// Hamburger Menu
+let item = document.querySelector(".icon-hamburger");
+item.addEventListener("click", function () {
+  document.body.classList.toggle("menu-open");
+});
+if (screen.width < 768) {
+  script.setAttribute("async", "true");
+} else {
+  script.setAttribute("defer", "true");
+}
